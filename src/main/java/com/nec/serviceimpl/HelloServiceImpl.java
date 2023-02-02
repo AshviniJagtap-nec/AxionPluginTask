68476d4c912445bf3f5929ea48ba1e63da499f25
AshviniJagtap-nec <AshviniJagtap@theiet.org>
2023-02-02 19:32:41 +0530

    say hello method added

--- src/main/java/com/nec/serviceimpl/HelloServiceImpl.java
+++ src/main/java/com/nec/serviceimpl/HelloServiceImpl.java
@@ -2,7 +2,12 @@ package com.nec.serviceimpl;
 
 public class HelloServiceImpl {
 
+	private static void sayHello() {
+		System.out.println("Hey, Ashvini here..");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hey Hello..");
+		sayHello();
 	}
 }
